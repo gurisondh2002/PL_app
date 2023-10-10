ab313b1f3a3002e4717731f12a1bc9f2808989cf
Gurjeet <gurisondh2002@gmail.com>
2023-10-10 14:05:31 +0530

    Home Page Banner created

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { Herr_Von_Muellerhoff } from 'next/font/google'
+import PageBanner from '../components/Home/PageBanner/pagebanner'
 
 export default function Home() {
   return (
-    <div>hello</div>
+    <div>
+      <PageBanner />
+    </div>
   )
 }
