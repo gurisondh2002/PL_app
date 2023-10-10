a5c2090dea7dce446ecd8c4abd6fe666387dea51
Gurjeet <gurisondh2002@gmail.com>
2023-10-10 16:00:50 +0530

    Home Page footer section created

--- client/app/page.js
+++ client/app/page.js
@@ -4,6 +4,8 @@ import { Herr_Von_Muellerhoff } from 'next/font/google'
 import PageBanner from '../components/Home/PageBanner/pagebanner'
 import Services from '../components/Home/Services/services'
 import ProdsAndSers from '@/components/Home/ProdsAndSers/prodsandsers'
+import Trending from '@/components/Home/Trending/trending'
+import Footer from '@/components/Footer/footer'
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
       <PageBanner />
       <Services />
       <ProdsAndSers/>
+      <Trending/>
+      <Footer/>
     </div>
   )
 }
